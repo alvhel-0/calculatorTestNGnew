390620224b1a8162bd001a2272e2490e444a42e1
alvhel-0 <102586408+alvhel-0@users.noreply.github.com>
2022-05-18 11:46:00 +0200

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,20 +1,21 @@
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CalculatorTest {
 
     @Test
-    public void testSubtraction() {
+    void subtraction() {
     Calculator calculator = new Calculator();
-    int result = calculator.subtraction(35, 15);
-    assertEquals(result, 20);
+    Assert.assertEquals(20, calculator.subtraction(35, 15));
+
     }
 
     @Test
-    public void testMultiplication() {
+    void multiplication() {
         Calculator calculator = new Calculator();
-        int result = calculator.multiplication(3, 4);
-        assertEquals(result, 12);
+         Assert.assertEquals(12, calculator.multiplication(3,4));
+
     }
 }
